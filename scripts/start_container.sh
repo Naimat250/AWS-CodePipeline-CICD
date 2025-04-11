18b13f9feef8112ff692dfe936117b1a7a890aa6
Muhammad Naimat Ullah <65623633+Naimat250@users.noreply.github.com>
2025-04-11 16:56:38 +0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull naimat1233/simple-python-flask-app:latest
+# Pull the Docker image from ECR
+docker pull 133707302068.dkr.ecr.us-west-2.amazonaws.com/flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 naimat1233/simple-python-flask-app:latest
+docker run -d -p 5000:5000 133707302068.dkr.ecr.us-west-2.amazonaws.com/flask-app:latest
