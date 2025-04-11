f1637d60f01b96f5f61407238936b07975f757cb
Muhammad Naimat Ullah <65623633+Naimat250@users.noreply.github.com>
2025-04-11 16:56:21 +0500

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Get the container ID running the image and store in a variable
-CONTAINER_ID=$(docker ps | awk '/naimat1233\/simple-python-flask-app/ {print $1}')
+CONTAINER_ID=$(docker ps | awk '133707302068.dkr.ecr.us-west-2.amazonaws.com/flask-app:latest {print $1}')
 
 # If a container ID was found, stop and remove it
 if [ -n "$CONTAINER_ID" ]; then
@@ -13,5 +13,5 @@ if [ -n "$CONTAINER_ID" ]; then
   echo "Removing container: $CONTAINER_ID"
   docker rm "$CONTAINER_ID"
 else
-  echo "No running container found for naimat1233/simple-python-flask-app"
+  echo "No running container found for 133707302068.dkr.ecr.us-west-2.amazonaws.com/flask-app:latest"
 fi
